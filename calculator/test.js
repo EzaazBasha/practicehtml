0e35db6fd294bf779c9a66fd465c603eb782788d
EzaazBasha <ezaazbasha786@gmail.com>
2025-02-19 18:12:11 +0530

    Added Js file and now calculatoris completed.

--- calculator/test.js
+++ calculator/test.js
@@ -1,3 +1,13 @@
 function display(val){
     document.getElementById('input').value+=val;
 }
+
+function clearscreen(){
+    document.getElementById('input').value="";
+}
+
+function calc(){
+    var x = document.getElementById('input').value
+    var y = eval(x);
+    document.getElementById('input').value=y;
+}
